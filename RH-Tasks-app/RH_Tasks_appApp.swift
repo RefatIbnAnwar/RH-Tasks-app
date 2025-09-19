607632c89546e2ceeda1ee27fad66e3a1857e055
Md Refat Hossain <refatibnanwar@gmail.com>
2025-09-19 11:49:03 +0800

    Initial Commit

---
+++ RH-Tasks-app/RH_Tasks_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  RH_Tasks_appApp.swift
+//  RH-Tasks-app
+//
+//  Created by Md Refat Hossain on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RH_Tasks_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
